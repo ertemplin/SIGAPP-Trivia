50ba92bf0b84c7b80abf4c941b6cf04134e9cc20
Eric Templin <ertemplin@gmail.com>
2012-02-28 23:41:17 -0500

    Created a basic rails application using "rails new ."

---
+++ RubyOnRails-TriviaServer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyOnRailsTriviaserver::Application.config.secret_token = 'f2e5fdec26051722f8a0d8ae501d5d68c947d74a1d53220fe3b1d078bdd80d1b0207a0b739311f56181c42e2187e26f57c7f4c623bb2434f26e526af8572e616'
