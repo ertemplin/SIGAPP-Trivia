4dbcc60c373ba7c58469367d256b3b5024c44ab4
Eric Templin <ertemplin@gmail.com>
2012-03-05 17:13:40 -0500

    Added field validation

--- trivia-server/app/models/question.rb
+++ trivia-server/app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ActiveRecord::Base
+	validates :question, :presence => true
+	validates :correctAnswer, :presence => true
+	validates :incorrect1, :presence => true
+	validates :incorrect2, :presence => true
+	validates :incorrect3, :presence => true
 end
