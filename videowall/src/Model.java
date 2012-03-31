fcf08ab894e542bdc2029c2e55a4f9d354a3c01c
Eric Templin <ertemplin@gmail.com>
2012-03-31 16:42:23 -0400

    Added model class.

---
+++ videowall/src/Model.java
@@ -0,0 +1,28 @@
+
+
+import java.util.Observable;
+import java.util.ArrayList;
+
+public class Model extends Observable
+{
+    static ArrayList<String> strings = new ArrayList<String>();
+    
+    void appendLine(String line) {
+        strings.add(line);
+        setChanged();
+        notifyObservers(line);
+    }
+    
+    void clear() {
+        strings.clear();
+        setChanged();
+        notifyObservers(null);
+    }
+    
+    void initialHack() {
+        for (int i = 0; i < strings.size(); i++) {
+            setChanged();
+            notifyObservers(strings.get(i));
+        }
+    }
+}
