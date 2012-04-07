cc4e0812be1903de03e31c43b7f372981517ee00
Eric Templin <ertemplin@gmail.com>
2012-04-07 17:38:27 -0400

    Cleaned up UI, have basic network connectivity.

--- BuzzTime/src/com/sigapp/buzztime/BuzzTimeActivity.java
+++ BuzzTime/src/com/sigapp/buzztime/BuzzTimeActivity.java
@@ -1,8 +1,17 @@
 package com.sigapp.buzztime;
 
+import java.util.Observable;
+import java.util.Observer;
+
+import edu.purdue.cs.Message;
+import edu.purdue.cs.RoomConnection;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -12,6 +21,7 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BuzzTimeActivity extends Activity {
 	
@@ -19,6 +29,8 @@ public class BuzzTimeActivity extends Activity {
 	 
 	 TextView text_uname;
 	 TextView text_score;
+	 
+	 RoomConnection room;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,61 +61,60 @@ public class BuzzTimeActivity extends Activity {
 			}
 
 			@Override
-			public View getView(int position, View convertView, ViewGroup parent) {
+			public View getView(final int position, View convertView, ViewGroup parent) {
 				// TODO Auto-generated method stub
 				Button b = new Button(BuzzTimeActivity.this);
 				b.setHeight(300);
 				b.setText((char)('A' + position) + "");
-				
+				b.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View arg0) {
+						Log.d("Click debugging", ((char)('A' + position))+"");
+						
+					}
+				});
 				return b;
 			}
         	
         });
         
-        final Dialog d = new Dialog(this);
-        LinearLayout l = new LinearLayout(this);
-		TextView uname = new TextView(BuzzTimeActivity.this);
-		TextView code = new TextView(this);
-		final EditText e_uname = new EditText(this);
-		EditText e_code = new EditText(this);
-		Button submitDialog = new Button(this);
-		l.addView(uname);
-		l.addView(e_uname);
-		l.addView(code);
-		l.addView(e_code);
-		l.addView(submitDialog);
-		l.setOrientation(LinearLayout.VERTICAL);
-		uname.setText("User Name: ");
-		code.setText("Code");
-		submitDialog.setText("Submit");
-	    d.setCancelable(false);
-		
-		submitDialog.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				userName = e_uname.getText().toString();
-				d.dismiss();
-				text_uname.setText("User Name:" + " " + userName);
-		        text_score.setText("Score: " + "0");
-				
-				
-			}
-			
-		
-		}
-		
-		
-				);
-		
-		
-		
-        
-		d.setContentView(l);
-		
-		d.show();
-		
-        
+        createJoinDialog();
     }
+    
+    public void createJoinDialog() {
+    	final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+    	alertDialogBuilder.setMessage("New Game");
+    	alertDialogBuilder.setCancelable(false);
+    	LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+    	final View layout = inflater.inflate(R.layout.join_game_dialog,(ViewGroup) findViewById(R.id.dialog_root));
+    	alertDialogBuilder.setView(layout);
+    	final EditText userNameBox = (EditText) layout.findViewById(R.id.unameInput);
+    	
+    	alertDialogBuilder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				//EditText userNameBox = (EditText) layout.findViewById(R.id.unameInput);
+        		String userName = userNameBox.getText().toString();
+        		text_uname.setText("User Name:" + " " + userName);
+        		text_score.setText("Score: " + "0");
+        		
+        		room = new RoomConnection(userName, new Observer() {
+					@Override
+					public void update(Observable arg0, Object arg1) {
+						Message m = (Message) arg1;
+						Log.d("New Message", m.getText());
+						
+					}
+        		});
+			}
+    		
+    	});
+
+    	
+    	
+    	AlertDialog joinGameDialog = alertDialogBuilder.create();
+
+		joinGameDialog.show();
+    }
+    
 }
