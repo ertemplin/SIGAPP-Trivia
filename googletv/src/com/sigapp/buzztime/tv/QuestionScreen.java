d056567b97f20a1ab75627dc96a24c2393a69bfc
Jaye <jfranklin773@gmail.com>
2012-04-11 19:46:15 -0400

    added initial prototype of question screen

---
+++ googletv/src/com/sigapp/buzztime/tv/QuestionScreen.java
@@ -0,0 +1,14 @@
+package com.sigapp.buzztime.tv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuestionScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.questionscreen);
+	}
+
+}
