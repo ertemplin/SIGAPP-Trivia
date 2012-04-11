8bfab5032211c2d2433e535feb6580ec4b0c818d
Yu Bhin Choi <choi5@pod1-2.cs.purdue.edu>
2012-04-11 17:19:24 -0400

    Updated stuff

--- videowall/src/RoomObserver.java
+++ videowall/src/RoomObserver.java
@@ -3,23 +3,12 @@ import java.util.Observer;
 import java.util.Scanner;
 import edu.purdue.cs.Message;
 
-class RoomObserver implements Observer
+public class RoomObserver implements Observer
 {
-	@Override
-	public void update(Observable arg0, Object arg1)
-	{
-		Message message = (Message) arg1;
-		String text = message.getText();
-		Model model = new Model();
-		Scanner in = new Scanner(text);
-		if (in.hasNextLong())
-		{
-			long t = in.nextLong();
-			long delay = System.currentTimeMillis() - t;
-			message.setText(in.nextLine().trim());
-			model.appendLine(String.format("%s [rtt %d ms]", message, delay));
-		}
-		else
-			model.appendLine(message.toString());
-	}
+    public void update(Observable arg0, Object arg1)
+    {
+	Message message = (Message) arg1;
+	String text = message.getText();
+	System.out.println(text);
+    }
 }
